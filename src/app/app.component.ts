0f15258e97a0e6d5611cfe3e8889b575416febe9
OrlandoYV <sergioyv@gmail.com>
2024-06-01 12:04:44 -0500

    oy - counter encapsulado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Hallo Welt';
-  public counter: number = 20;
-
-  increaseBy(): void {
-    this.counter = this.counter + 1;
-  }
-
-  decreaseBy(): void {
-    this.counter = this.counter - 1;
-  }
-
-  reset(): void {
-    this.counter = 10;
-  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter 1234</h1>',
+  template: `
+    <h1>...Hello Counter...</h1>
+    <h3>Counter: {{ counter }}</h3>
+
+    <button (click)="this.increaseBy()">+1</button>
+    <button (click)="this.decreaseBy()">-1</button>
+    <button (click)="this.reset()">Reset</button>
+  `,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  constructor() {}
+
+  public counter: number = 20;
+
+  increaseBy(): void {
+    this.counter = this.counter + 1;
+  }
+
+  decreaseBy(): void {
+    this.counter = this.counter - 1;
+  }
+
+  reset(): void {
+    this.counter = 10;
+  }
+}
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-counter',
+//   template: '<h1>Hola Counter 1234</h1>',
+// })
+// export class CounterComponent {}
