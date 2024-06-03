3f1acb77b08245862066e1ee41769f115ec95937
OrlandoYV <sergioyv@gmail.com>
2024-06-03 15:34:14 -0500

    oy - update props with functions

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironMan';
   public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  public getHeroDescription(): string {
+    return `${this.name.toLowerCase()} - ${this.age} `;
+  }
+
+  public changeHero(newName: string): void {
+    this.name = newName;
+  }
+
+  public changeAge(newAge: number): void {
+    this.age = newAge;
+  }
 }
