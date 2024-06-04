7c65aecba0d5cdc658ecb59d14efc150aa2a52ca
OrlandoYV <sergioyv@gmail.com>
2024-06-04 14:22:06 -0500

    oy - modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './counter/components/counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeroComponent, ListComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
