aebc8ce6c1e9b0ad81f54fb6aa29de7f92969bdf
OrlandoYV <sergioyv@gmail.com>
2024-06-05 16:46:16 -0500

    oy - list dbz heros component

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPage } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [MainPage],
+  declarations: [MainPage, ListComponent],
   imports: [CommonModule],
   exports: [MainPage],
 })
