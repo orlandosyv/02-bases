f64e341da640f2817ec8fdd2ade290891ad2b857
OrlandoYV <sergioyv@gmail.com>
2024-06-05 14:51:41 -0500

    oy - html for main page

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPage } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [MainPage],
+  imports: [CommonModule],
+  exports: [MainPage],
+})
+export class DbzModule {}
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-component-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPage {
+  constructor() {}
+
+  ngOnInit() {}
+}
