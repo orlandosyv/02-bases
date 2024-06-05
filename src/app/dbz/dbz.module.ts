360a468f2a434a3ea0ab93a70739f2618b456048
OrlandoYV <sergioyv@gmail.com>
2024-06-05 18:42:03 -0500

    oy - character component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPage } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
-  declarations: [MainPage, ListComponent],
+  declarations: [MainPage, ListComponent, CharacterComponent],
   imports: [CommonModule],
   exports: [MainPage],
 })
