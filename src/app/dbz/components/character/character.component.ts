0db509edece21667db0d95110cdc43dacf81eeb7
OrlandoYV <sergioyv@gmail.com>
2024-06-05 18:47:47 -0500

    oy - edits on character

--- src/app/dbz/components/character/character.component.ts
+++ src/app/dbz/components/character/character.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-character',
+  selector: 'dbz-add-character',
   templateUrl: './character.component.html',
-  styleUrl: './character.component.css'
+  styleUrl: './character.component.css',
 })
-export class CharacterComponent {
-
-}
+export class CharacterComponent {}
