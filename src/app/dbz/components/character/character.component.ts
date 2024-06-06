ef0e84f46cf0c13d40832b09e633e419aa8bada8
OrlandoYV <sergioyv@gmail.com>
2024-06-06 15:11:47 -0500

    oy - formsModule y ngModel

--- src/app/dbz/components/character/character.component.ts
+++ src/app/dbz/components/character/character.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './character.component.html',
   styleUrl: './character.component.css',
 })
-export class CharacterComponent {}
+export class CharacterComponent {
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
+
+  emitCharacter() {
+    console.log(this.character);
+  }
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainPage } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { CharacterComponent } from './components/character/character.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MainPage, ListComponent, CharacterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [MainPage],
 })
 export class DbzModule {}
